706bf3d8547c8914dc64674ac78f645254b6ad2f
Mykola <3277207+kolya182@users.noreply.github.com>
2018-09-07 12:17:41 -0700

    Test if address start with 0x and have total length 42

--- test/index.js
+++ test/index.js
@@ -12,10 +12,17 @@ const isAddress = address => addressExp.test(address)
 
 const imageFileNames = fs.readdirSync('./images')
 
-imageFileNames.forEach(n => { 
-    if (!imgExp.test(n)) {
-        exitWithMsg(`${n} image must be png`)
+imageFileNames.forEach(image => { 
+    if (!imgExp.test(image)) {
+        exitWithMsg(`${image} image must be png`)
     } 
+
+    if (image) {
+        const address =  image.replace(/.png/g, '')
+        if (!addressExp.test(address) && !image.startsWith('ethereum')) {
+            exitWithMsg(`${address} image must have length 42 instead have ${address.length} or start with '0x'`)
+        }
+    }
 })
 
 console.log(`Passed all tests`)
