2474bb18e793d4644b25a044c15f28330462053b
Mykola <3277207+kolya182@users.noreply.github.com>
2018-09-07 11:19:35 -0700

    In lowercase

--- test/index.js
+++ test/index.js
@@ -1,19 +1,21 @@
 const fs = require('fs')
 
 const imgExp = /\.png$/
+const addressExp = /^(0x)?[0-9a-f]{40}$/i
 
 const exitWithMsg = (msg) => {
     console.log(msg)
     process.exit(1)
 }
 
+const isAddress = address => addressExp.test(address)
+
 const imageFileNames = fs.readdirSync('./images')
 
-imageFileNames.forEach(n => {
-    if (imgExp.test(n)) {
-    } else {
-      exitWithMsg(`${n} image must be png`)
-    }
+imageFileNames.forEach(n => { 
+    if (!imgExp.test(n)) {
+        exitWithMsg(`${n} image must be png`)
+    } 
 })
 
 console.log(`Passed all tests`)
