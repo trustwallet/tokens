35a6aaf95008334cc8593d8066006dfbb6829915
Mykola <3277207+kolya182@users.noreply.github.com>
2018-09-18 15:14:37 -0700

    Add a few checks
    Make sure address starts from 0x
    Make sure address has no uppercase characters

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
 
 const imgExp = /\.png$/
-const addressExp = /^(0x)?[0-9a-f]{40}$/i
+const upperCaseExp = /[A-F]/
+const OxExp = /^(0x)/
+const addressExp = /[0-9a-f]{40}$/i
 
 const exitWithMsg = (msg) => {
     console.log(msg)
@@ -10,18 +12,27 @@ const exitWithMsg = (msg) => {
 
 const isAddress = address => addressExp.test(address)
 
+const remoteExtension = string => string.replace(/.png/g, '')
+
 const imageFileNames = fs.readdirSync('./images')
 
-imageFileNames.forEach(image => { 
+imageFileNames.forEach(image => {
+    const address = remoteExtension(image)
+
     if (!imgExp.test(image)) {
         exitWithMsg(`${image} image must be png`)
     } 
+    
+    if (upperCaseExp.test(address)) {
+        exitWithMsg(`${address} image must be in lowercase`)
+    }
 
-    if (image) {
-        const address =  image.replace(/.png/g, '')
-        if (!addressExp.test(address) && !image.startsWith('ethereum')) {
-            exitWithMsg(`${address} image must have length 42 instead have ${address.length} or start with '0x'`)
-        }
+    if (!OxExp.test(address) && !address.startsWith('ethereum')) {
+        exitWithMsg(`${address} must start with 0x`)
+    }
+
+    if (!isAddress(address) && !address.startsWith('ethereum')) {
+        exitWithMsg(`${address} image must have length 42 instead have ${address.length}`)
     }
 })
 
