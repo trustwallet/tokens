ebbd4aadb8d6b4a232cec61260b10dcd2ab2bb45
Mykola <3277207+kolya182@users.noreply.github.com>
2019-08-08 17:17:03 -0700

    Migration (#2224)
    * Remove tmp folders
    
    * Remove
    
    * Fix broken files
    
    * Remove .DS_Store files recursivly
    
    * Remove deprecated BEP2
    
    * Update tests for new structure
    
    * Add Torriczetta
    
    * Update node

--- test/verifyTokens.js
+++ test/verifyTokens.js
@@ -3,10 +3,14 @@ const fs = require('fs')
 
 const TRUST_API = `https://api.trustwallet.com/tokens/verification`
 const TOKEN_VERIFICATION_KEY = process.env.TOKEN_VERIFICATION_KEY
+const ethereumSidechains = ["ethereum", "classic", "poa", "tomochain", "gochain", "wanchain", "callisto", "thundertoken"]
 
-const tokens = fs.readdirSync('./tokens')
-const addresses = tokens.map(token => token.replace('.png', '').toLowerCase())
-axios.post(TRUST_API, {tokens: addresses}, {
+const addresses = []
+ethereumSidechains.forEach(chain => {
+    addresses.push(fs.readdirSync(`./blockchains/${chain}/assets`))
+})
+
+axios.post(TRUST_API, {tokens: addresses.flat()}, {
     headers: {
         TOKEN_VERIFICATION_KEY
     }
